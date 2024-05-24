cccb5bef5a628ab054f0aa7f07574b866b8c33a5
yonzro1610 <yonzro1610@proton.me>
2024-05-24 04:45:34 -0600

    Update main.py

--- main.py
+++ main.py
@@ -16,9 +16,52 @@ class color:
    BOLD = '\033[1m'
    UNDERLINE = '\033[4m'
    END = '\033[0m'
+   
+just_fix_windows_console()
 
 # Clean console
 os.system('cls'); os.system('title YCLVV')
 
 # Get URL
-providedLink = input(f"{color.BLUE}{color.UNDERLINE}Enter Channel URL > "); print(color.END)
+providedLink = input(f"{color.BLUE}{color.UNDERLINE}Enter Channel URL > ")
+
+# Functions
+def scrapeVideos(url):
+    ydl_opts = {
+        'extract_flat': True,
+        'skip_download': True,
+        'quiet': True
+    }
+    
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        result = ydl.extract_info(channel_url, download=False)
+    
+    videos = []
+    
+    if 'entries' in result:
+        for entry in result['entries']:
+            if 'url' in entry:
+                vurl = entry['url']
+                vinfo = ydl.extract_info(vurl, download=False)
+                vdetails = {
+                    'title': vinfo.get('title'),
+                    'views': vinfo.get('views'),
+                    'url': vurl,
+                    'upload_date': vinfo.get('upload_date')
+                }
+                videos.append(vdetails)
+        
+        return videos
+    else:
+        print("This channe has no videos.")
+
+# Validate URL
+regex = re.compile(r"https:\/\/www\.youtube\.com\/channel\/\S+")
+valid = regex.match(providedLink)
+if valid:
+    print("Link valid!")
+    
+else:
+    print("Invalid link.")
+
+print(color.END)
