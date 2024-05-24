42230e4b803a494629df4e245c4ed8e1eb5ad057
yonzro1610 <yonzro1610@proton.me>
2024-05-23 23:53:12 -0600

    upd

---
+++ src/app.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+
+process.stdin.setEncoding('utf8');
+
+function generateRandomString(length) {
+    const charset = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let result = '';
+    for (let i = 0; i < length; i++) {
+        const randomIndex = Math.floor(Math.random() * charset.length);
+        result += charset[randomIndex];
+    }
+    return result;
+}
+
+process.stdout.write('Enter channel link: ');
+
+process.stdin.on('data', function(data) {
+    const link = data.trim();
+    const file = generateRandomString('6') + '.tmp'
+    fs.writeFile(file, link, function(err) {
+        if (err) {
+            console.error('Error writing to file:', err);
+        } else {
+            console.log('Data has been written to', file);
+        }
+    });
+
+});
